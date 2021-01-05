aef07c54a4df7fa2bc89314b5b5781eb1b4a1384
Christopher Patrick Tortora <ctortora42@gmail.com>
2021-01-05 13:14:17 -0500

    Needs greyscale and final UI touches

--- script.js
+++ script.js
@@ -2,18 +2,60 @@ const header = document.querySelector("header");
 const size = document.createElement('p');
 size.textContent = "16 x 16"
 header.appendChild(size);
+let blackFlag = null;
+let rainbowFlag = null;
 
 const container = document.getElementById("container");
 container.classList.add("container");
 
 var rangeInput = document.getElementById("rangeInput");
 
-createDivs(16);
+createDivsBlack(16);
 
 var divList;
 
 //functions
-function createDivs(divByDiv){
+function createDivsBlack(divByDiv){
+    blackFlag = true;
+    rainbowFlag = false;
+    const numDivs = divByDiv * divByDiv;
+    const divSize = (( 1 / divByDiv) * 100);
+    console.log(divSize);
+    for(i = 1; i < numDivs + 1; i++){
+        const div = document.createElement("div");
+    
+        div.classList.add("box");
+        div.id = "box"
+        
+       
+        div.style.width = divSize + "%";
+        div.style.height = divSize + "%";
+
+       
+        if(((i - 1) % divByDiv) == 0){
+            div.classList.add("nextRowBox")
+        }
+      
+        container.appendChild(div);
+    }
+
+    divList = document.querySelectorAll('#box');
+
+    divList.forEach((div) => {
+        div.addEventListener('mouseenter', (e) => {
+            console.log(div.style.backgroundColor);
+            if(div.style.backgroundColor === ""){
+            div.style.backgroundColor = 'rgb(0, 0, 0)';
+            }
+        })
+    }) 
+
+
+}
+
+function createDivsRainbow(divByDiv){
+    rainbowFlag = true;
+    blackFlag = false;
     const numDivs = divByDiv * divByDiv;
     const divSize = (( 1 / divByDiv) * 100);
     console.log(divSize);
@@ -73,7 +115,13 @@ function shake(){
 
 rangeInput.addEventListener('mouseup', function() {
     shake();
-    createDivs(rangeInput.value);
+    if (blackFlag == true){
+        createDivsBlack(rangeInput.value);
+    }
+    else if(rainbowFlag == true){
+        createDivsRainbow(rangeInput.value);
+    }
+    
     size.textContent = rangeInput.value + " x " + rangeInput.value;
     console.log(rangeInput.value);
 }); 
@@ -82,10 +130,26 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        if(button.id == "Shake"){
+        if(button.id == "Black"){
+            
             shake();
-            createDivs(rangeInput.value);
+            createDivsBlack(rangeInput.value);
            
+        }
+        else if(button.id == "Shake"){
+            shake();
+            if (blackFlag == true){
+                createDivsBlack(rangeInput.value);
+            }
+            else if(rainbowFlag == true){
+                createDivsRainbow(rangeInput.value);
+            }
+
+        }
+        else if(button.id == "Rainbow"){
+            
+            shake();
+            createDivsRainbow(rangeInput.value);
         }
         /*else if(button.textContent == "Black"){
             divList.forEach((div) => {
