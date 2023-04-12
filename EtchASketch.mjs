4e3cb7cde6ebd6c010dfbf605611040fc409b2bc
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-04-12 12:55:10 -0400

    Create drawing functionality

---
+++ EtchASketch.mjs
@@ -0,0 +1,30 @@
+const EtchASketch = () => {
+
+    const createDivs = (size) => {
+        const area = size**2;
+        const dimension = (1 / Math.sqrt(area)) * 100
+        const screen = document.querySelector('main')
+        for(let i = 0; i < area; i++){
+            const div = document.createElement('div');
+            div.style.width = `${dimension}%` 
+            div.style.height = `${dimension}%` 
+            div.classList.add('pixel')
+            screen.append(div)
+        }
+
+    }
+
+    const drawListener = () => {
+        const pixels = document.querySelectorAll('.pixel')
+        pixels.forEach(pixel => {
+            pixel.addEventListener('mouseover', () => {
+                const opacity = window.getComputedStyle(pixel).opacity
+                pixel.style.opacity = opacity - .1
+            })
+        })
+    };
+
+    return { createDivs, drawListener }
+}
+
+export { EtchASketch }
---
+++ main.mjs
@@ -0,0 +1,6 @@
+import { EtchASketch } from "./EtchASketch.mjs";
+
+const etchASketch = EtchASketch();
+
+etchASketch.createDivs(32)
+etchASketch.drawListener()
