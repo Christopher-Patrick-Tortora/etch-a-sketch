bcbc0be685e06bfdf4ee0d6f767d248cfc4d2aa1
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-04-12 18:58:19 -0400

    Add shake animation and settings listener

--- EtchASketch.mjs
+++ EtchASketch.mjs
@@ -1,14 +1,14 @@
 const EtchASketch = () => {
 
     const createDivs = (size) => {
-        const area = size**2;
+        const area = size ** 2;
         const dimension = (1 / Math.sqrt(area)) * 100
         const screen = document.querySelector('main')
-        for(let i = 0; i < area; i++){
+        for (let i = 0; i < area; i++) {
             const div = document.createElement('div');
             div.id = i + 1
-            div.style.width = `${dimension}%` 
-            div.style.height = `${dimension}%` 
+            div.style.width = `${dimension}%`
+            div.style.height = `${dimension}%`
             div.classList.add('pixel')
             screen.append(div)
         }
@@ -21,34 +21,46 @@ const EtchASketch = () => {
     }
 
     const colorClassic = (pixel) => {
-        if(pixel){
-        const opacity = window.getComputedStyle(pixel).opacity
-        pixel.style.opacity = opacity - .1
+        if (pixel) {
+            const opacity = window.getComputedStyle(pixel).opacity
+            pixel.style.opacity = opacity - .1
         }
     }
 
     const colorBlack = (pixel) => {
-        if(pixel){
+        if (pixel) {
             pixel.style.backgroundColor = 'black'
         }
     }
 
     const colorRandom = (pixel) => {
-        if(pixel){
-            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        if (pixel) {
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
             const backgroundColor = pixel.style.backgroundColor
-            console.log(backgroundColor)
-            console.log(backgroundColor)
-            if(backgroundColor == ""){
+            if (backgroundColor == "") {
                 pixel.style.backgroundColor = `#${randomColor}`
             }
         }
     }
 
     const selectColor = (color) => {
-        if(color.id === 'classic') drawListener(colorClassic)
-        else if(color.id === 'black') drawListener(colorBlack)
-        else if(color.id === 'random') drawListener(colorRandom)
+        if (color === 'classic') drawListener(colorClassic)
+        else if (color === 'black') drawListener(colorBlack)
+        else if (color === 'random') drawListener(colorRandom)
+    }
+
+    const shakeListener = () => {
+        const shakeButton = document.querySelector('.button__shake')
+        const etchASketch = document.querySelector('.etch-a-sketch')
+        shakeButton.addEventListener('click', () => {
+            etchASketch.classList.add('shake')
+            shakeButton.classList.add('button__pushed')
+            //time should be equal to animation length
+            setTimeout(() => {
+                etchASketch.classList.toggle('shake')
+                shakeButton.classList.toggle('button__pushed')
+            }, 1000)
+        })
     }
 
     const drawListener = (color) => {
@@ -60,56 +72,50 @@ const EtchASketch = () => {
         })
     };
 
-    const areaListener = () => {
-        const areaContainer = document.querySelector(('.container__area'))
-        const buttons = areaContainer.querySelectorAll('button')
+    const settingsListener = () => {
+        const buttons = document.querySelectorAll('button');
 
         buttons.forEach(button => {
             button.addEventListener('click', () => {
+                const areaContainer = document.querySelector(('.container__area'))
+                const areaSelected = areaContainer.querySelector('.button__pushed')
+
                 const colorContainer = document.querySelector(('.container__color'))
                 const colorSelected = colorContainer.querySelector('.button__pushed')
 
-                if(!button.classList.contains('.button__pushed')){
-                    const pushedButton = areaContainer.querySelector('.button__pushed')
-                    pushedButton.classList.remove('button__pushed')
+                if (!button.classList.contains('button__pushed')) {
+                    const clickedButtonContainer = button.parentElement.parentElement;
+                    const clickedSettingContainer = clickedButtonContainer.parentElement
+                    const prevPushedButton = clickedButtonContainer.querySelector('.button__pushed')
+
+                    //swaps pushed buttons
+                    prevPushedButton.classList.remove('button__pushed')
                     button.classList.add('button__pushed')
+
+                    //variables depend on which setting is clicked
+                    const size = clickedSettingContainer.classList.contains('container__area') ? 
+                    button.id : areaSelected.id
+                    const color = clickedSettingContainer.classList.contains('container__color') ? 
+                    button.id : colorSelected.id
+            
                     removeDivs()
-                    createDivs(button.id)
-                    selectColor(colorSelected)
-
+                    createDivs(size)
+                    selectColor(color)
                 }
-            } )
+            })
         })
-    }
 
-    const colorListener = () => {
-        const colorContainer = document.querySelector(('.container__color'))
-        const buttons = colorContainer.querySelectorAll('button')
-
-        const areaContainer = document.querySelector(('.container__area'))
-        const areaSelected = areaContainer.querySelector('.button__pushed')
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                if(!button.classList.contains('.button__pushed')){
-                    const pushedButton = colorContainer.querySelector('.button__pushed')
-                    pushedButton.classList.remove('button__pushed')
-                    button.classList.add('button__pushed')
-                    removeDivs()
-                    createDivs(areaSelected.id)
-                    selectColor(button)
-                    
-                }
-            } )
-        })
     }
 
     const initialize = () => {
-//divs above 62 create instability therefore keep at 62 but say 64 because of style
+        //divs above 62 create instability therefore keep at 62 but say 64 because of style
         createDivs(62)
         drawListener(colorClassic)
-        areaListener()
-        colorListener()
-    } 
+        //areaListener()
+        //colorListener()
+        shakeListener()
+        settingsListener()
+    }
 
     return { initialize }
 }
